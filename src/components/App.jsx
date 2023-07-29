e7f36614461ba9bd8b3f036181693a1eccce5314
ZorAnderius <gabriel.zorander@gmail.com>
2023-07-29 21:01:36 +0100

    add page and components folders with jsx and css files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,24 @@
+import { Home } from 'pages/Home';
+import { MovieDetails } from 'pages/MovieDetails';
+import { Movies } from 'pages/Movies';
+import { Link, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <div>
+      <nav className="">
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<div>Cast</div>} />
+          <Route path="/movies/:movieId/reviews" element={<div>Reviews</div>} />
+        </Route>
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
+    </div>
+  );
 };
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,7 @@
+export const MovieDetails = () => {
+  return (
+    <main>
+      <div>Movie info</div>
+    </main>
+  );
+};
